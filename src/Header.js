a675b1dbe97d777e600915a48b93140f759d7011
Yogesh9643 <rajputyogesh563@gmail.com>
2020-12-14 21:18:50 +0530

    added link elements to top navbar

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
-import SearchIcon from "@material-ui/icons/Search";
-
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 
 function Header() {
   return (
@@ -15,8 +15,38 @@ function Header() {
         />
       </Link>
       <div className="header__search">
-      <input type="text" className="header__searchInput"/>
-      <SearchIcon className="header__searchIcon"/>
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello There!</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header_optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </nav>
   )
