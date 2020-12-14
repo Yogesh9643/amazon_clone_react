578535b0aca0bce43db78d23661a057cd92f2f6f
Yogesh9643 <rajputyogesh563@gmail.com>
2020-12-14 20:29:57 +0530

    added amazon logo and searchbar to navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import './App.css'
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom'
+import Header from "./Header";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <div>Hey what's up guys !</div>
+        
         <Switch>
-          <Route path='/checkout'>
-           <h1>
-           This is checkout page
-             </h1> 
+          <Route path="/checkout">
+            <h1>This is checkout page</h1>
           </Route>
-          <Route path='/login'>
-            <h1>
-              This is login page
-            </h1>
-
+          <Route path="/login">
+            <h1>This is login page</h1>
           </Route>
-          <Route path='/'>
-            <h1>This is the homepage</h1> 
-
+          <Route path="/">
+            <Header/>
+            <h1>This is the homepage</h1>
           </Route>
-          
         </Switch>
       </div>
     </Router>
