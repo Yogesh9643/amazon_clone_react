f3220c2f7f8b021a1bb17d56c864187ce8345e48
Yogesh9643 <rajputyogesh563@gmail.com>
2020-12-14 18:01:12 +0530

    setting up the react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="app">
+        <div>Hey what's up guys !</div>
+        <Switch>
+          <Route path='/checkout'>
+           <h1>
+           This is checkout page
+             </h1> 
+          </Route>
+          <Route path='/login'>
+            <h1>
+              This is login page
+            </h1>
+
+          </Route>
+          <Route path='/'>
+            <h1>This is the homepage</h1> 
+
+          </Route>
+          
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
